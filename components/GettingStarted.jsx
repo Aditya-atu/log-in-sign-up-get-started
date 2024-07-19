648eddaa73a025636291acfcdbe363de49bc4201
Aditya-atu <adityaabhiseka@gmail.com>
2024-07-19 17:17:43 +0530

    Update GettingStarted.jsx

--- components/GettingStarted.jsx
+++ components/GettingStarted.jsx
@@ -1,33 +1,3 @@
-// GetStartedComponent.js
-/*
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
-const GetStartedComponent = ({ onStart }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Get Started</Text>
-      <Button onPress={onStart} title="Start Now" />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
-
-export default GetStartedComponent;
-*/
-
-
 import React from 'react';
 import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 import styles from './styles';
